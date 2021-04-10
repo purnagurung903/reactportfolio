7ab0bba049d0c6e8faae6128692dda5e9a77090f
Purna Gurung <purnagurung903@gmail.com>
2021-04-10 13:29:25 -0400

    first commit

---
+++ reactportfolio/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="row">
+      <div className="col-3">
+        <div className="app_sidebar">
+        sidebar
+        </div>
+      </div>
+      <div className="col-9 app_main-content">
+        main
+      </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
