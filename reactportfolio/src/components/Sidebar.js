57172090b85a5c7fcba5f31f29224e8e63eb3efe
Purna Gurung <purnagurung903@gmail.com>
2021-04-10 17:20:41 -0400

    done with sidebar component make resume and email link work

--- reactportfolio/src/components/Sidebar.js
+++ reactportfolio/src/components/Sidebar.js
@@ -5,9 +5,13 @@ import github from "../assets/icons/github.svg"
 import pin from "../assets/icons/pin.svg"
 import tie from "../assets/icons/tie.svg"
 import image from "../assets/Myphoto.png"
+import resume from "../assets/Resume.pdf"
 
 
 const Sidebar = () => {
+  const handleEmailMe = () => {
+    window.open("mailto:purnagurung903@gmail.com")
+  }
   return (
     <div className="sidebar">
         <img src={image} alt="avatar" className="sidebar__avatar"/>
@@ -15,8 +19,8 @@ const Sidebar = () => {
         </div>
         <div className="sidebar__item sidebar__title">Fullstack Developer
         </div>
-        <a href="!#">
-          <div className="sidebar__item">
+        <a href={resume} download="Resume.pdf">
+          <div className="sidebar__item sidebar__resume">
             <img src={tie} alt="resume" className="sidebar__icon"/>Download Resume
 
           </div>
@@ -27,13 +31,13 @@ const Sidebar = () => {
 
         </figure>
         <div className="sidebar__contact">
-          <div className="sidebar__item">
+          <div className="sidebar__item sidebar__github">
             <a href=" ">
-              <img src={github} alt="github"  className="sidebar__icon"/>github
+              <img src={github} alt="github"  className="sidebar__icon mr-3"/>github
             </a>
             </div>
             <div className="sidebar__location">
-              <img src={pin} alt="location" className="sidebar__icon" />
+              <img src={pin} alt="location" className="sidebar__icon mr-3" />
               New York, Queens
 
             </div>
@@ -42,7 +46,7 @@ const Sidebar = () => {
             <div className="sidebar__item">917-744-3403
             </div>
         </div>
-        <div className="sidebar__item sidebar__email">email me</div>
+        <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div>
     </div>
 
   )
