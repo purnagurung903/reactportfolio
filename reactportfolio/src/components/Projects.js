9f33feb8956cbb176b158e695b905dec88193432
Purna Gurung <purnagurung903@gmail.com>
2021-04-15 12:50:22 -0400

    added project component

--- reactportfolio/src/components/Projects.js
+++ reactportfolio/src/components/Projects.js
@@ -1,9 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import data_projects from "./data/projects_data"
+import ProjectCard from "./projectCard";
 const Projects = () => {
+  const [projects, setProjects] = useState(data_projects)
+  const handleFilterCategory = (name) => {
+    const new_array = data_projects.filter(project => project.category.includes(name))
+    setProjects(new_array)
+  }
   return (
-    <div>
-      <h1>this is Projects</h1>
+    <div className="container projects">
+      <div className="projects__navbar">
+         <div onClick={() => setProjects(data_projects)}>All</div>
+         <div onClick={() => handleFilterCategory("MySQL")}>MySQL</div>
+         <div onClick={() => handleFilterCategory("MongoDB")}>MongoDB</div>
+         <div onClick={() => handleFilterCategory("javascript")}>Javascript</div>
+         <div onClick={() => handleFilterCategory("Node")}>Node</div>
+      </div>
+      <div className="row">
+          {
+             projects.map(project => 
+              <ProjectCard key={project.name} project={project} />)
+          }
+      </div>
     </div>
   )
 }
---
+++ reactportfolio/src/components/data/projects_data.js
@@ -0,0 +1,52 @@
+import burger from "../../assets/projects_images/burger.png"
+import notetaker from "../../assets/projects_images/notetaker.png"
+import Quarentine from "../../assets/projects_images/Quarentine.png"
+import quickTravel from "../../assets/projects_images/quickTravel.png"
+import Timetracker from "../../assets/projects_images/Timetracker.png"
+import weatherapp from "../../assets/projects_images/weatherapp.png"
+
+const data_projects = [
+  {
+    name:"Quarantinis",
+    image:Quarentine,
+    deployed_url:"https://melissakinsey.github.io/Quarantinis/",
+    github_url:"https://github.com/melissakinsey/Quarantinis",
+    category: ["javascript"]
+  },
+  {
+    name:"Weather App",
+    image:weatherapp,
+    deployed_url:"https://purnagurung903.github.io/weatherApp/",
+    github_url:"https://github.com/purnagurung903/weatherApp.git",
+    category: ["javascript"]
+  },
+  {
+    name:"Workday Schedule",
+    image:Timetracker,
+    deployed_url:"https://purnagurung903.github.io/WorkDayScheduler/.",
+    github_url:"https://github.com/purnagurung903/WorkDayScheduler.git",
+    category: ["javascript"]
+  },
+  {
+    name:"Note Taker",
+    image:notetaker,
+    deployed_url:"https://aqueous-peak-19906.herokuapp.com/",
+    github_url:"https://github.com/purnagurung903/NoteTaker.git",
+    category: ["Node"]
+  },
+  {
+    name:"Quick_Travel",
+    image:quickTravel,
+    deployed_url:"https://thawing-peak-69215.herokuapp.com/",
+    github_url:"https://github.com/lindseybowen/Quick_Travel.git",
+    category: ["Node"]
+  },
+  {
+    name:"Burger App",
+    image:burger,
+    deployed_url:"https://arcane-shelf-93832.herokuapp.com/",
+    github_url:"https://github.com/purnagurung903/burger.git",
+    category: ["MySQL"]
+  },
+]
+export default data_projects;
